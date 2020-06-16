2a3f7aa93f30fe565f0f740be8b24fcdcf747577
SDuck4 <cotmddn4@gmail.com>
2020-06-16 14:08:08 +0900

    test: join() 테스트 작성

--- type-hangul.test.js
+++ type-hangul.test.js
@@ -31,3 +31,25 @@ describe('isHangul', () => {
     });
 
 });
+
+describe('join', () => {
+
+    describe('유효한 cho, jung, jong인 경우', () => {
+        let cho = 0,
+            jung = 18,
+            jong = 8;
+        test('합친 한글 반환', () => {
+            expect(TypeHangul.join(cho, jung, jong)).toBe('글');
+        });
+    });
+
+    describe('유효하지 않은 cho, jung, jong인 경우', () => {
+        let cho = -1,
+            jung = 99,
+            jong = 300;
+        test('빈 문자열 반환', () => {
+            expect(TypeHangul.join(cho, jung, jong)).toBe('');
+        });
+    });
+
+});
