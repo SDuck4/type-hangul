17aac200d99fe509a373ad80ac7340a6d75b5635
SDuck4 <cotmddn4@gmail.com>
2020-06-17 16:22:26 +0900

    test: test 함수 안에서 변수 선언

--- type-hangul.test.js
+++ type-hangul.test.js
@@ -3,29 +3,29 @@ const TypeHangul = require('./type-hangul');
 describe('isHangul', () => {
 
     describe('char가 한글인 경우', () => {
-        let char = '한';
         test('true 반환', () => {
+            let char = '한';
             expect(TypeHangul.isHangul(char)).toBe(true);
         });
     });
 
     describe('char가 자음인 경우', () => {
-        let char = 'ㅋ';
         test('false 반환', () => {
+            let char = 'ㅋ';
             expect(TypeHangul.isHangul(char)).toBe(false);
         });
     });
 
     describe('char가 모음인 경우', () => {
-        let char = 'ㅟ';
         test('false 반환', () => {
+            let char = 'ㅟ';
             expect(TypeHangul.isHangul(char)).toBe(false);
         });
     });
 
     describe('char가 영문인 경우', () => {
-        let char = 'A';
         test('false 반환', () => {
+            let char = 'A';
             expect(TypeHangul.isHangul(char)).toBe(false);
         });
     });
@@ -35,19 +35,19 @@ describe('isHangul', () => {
 describe('join', () => {
 
     describe('유효한 cho, jung, jong인 경우', () => {
-        let cho = 0,
-            jung = 18,
-            jong = 8;
         test('합친 한글 반환', () => {
+            let cho = 0;    // ㄱ
+            let jung = 18;  // ㅡ
+            let jong = 8;   // ㄹ
             expect(TypeHangul.join(cho, jung, jong)).toBe('글');
         });
     });
 
     describe('유효하지 않은 cho, jung, jong인 경우', () => {
-        let cho = -1,
-            jung = 99,
-            jong = 300;
         test('빈 문자열 반환', () => {
+            let cho = -1;
+            let jung = 99;
+            let jong = 300;
             expect(TypeHangul.join(cho, jung, jong)).toBe('');
         });
     });
