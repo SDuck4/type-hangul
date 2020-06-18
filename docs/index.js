0c84f178fc213d482b4bc108d541d2fcbee0ca61
SDuck4 <cotmddn4@gmail.com>
2020-06-18 23:30:43 +0900

    docs: home 섹션 subtitle에 타이핑 효과 적용

--- docs/index.js
+++ docs/index.js
@@ -6,4 +6,10 @@ $(function () {
         $('.navbar-menu').toggleClass('is-active');
     });
 
+    // #home-subtitle 타이핑 효과
+    TypeHangul.type('#home-subtitle', '한글 타이핑 효과 라이브러리', {
+        intervalType: 120,
+        intervalChar: 60,
+    });
+
 });
