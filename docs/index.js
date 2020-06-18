10e09cc1e66096dd186ff61b2353d6eb225dc37b
SDuck4 <cotmddn4@gmail.com>
2020-06-18 22:47:42 +0900

    docs: navbar burger menu 토글

---
+++ docs/index.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    // Bulma - toggle navbar burger menu
+    $('.navbar-burger').click(function () {
+        $('.navbar-burger').toggleClass('is-active');
+        $('.navbar-menu').toggleClass('is-active');
+    });
+
+});
