7d59a7897992660540691f943d3dea3783c00e09
SDuck4 <cotmddn4@gmail.com>
2020-06-26 09:51:24 +0900

    feat: 기본 humanize 함수

--- src/index.js
+++ src/index.js
@@ -172,6 +172,13 @@ function _setText(target, text) {
     }
 }
 
+// 기본 humanize 함수, number를 ratio 비율로 랜덤화해서 반환
+function _humanize(number, ratio) {
+    let min = number - number * ratio;
+    let max = number + number * ratio;
+    return Math.round(Math.random() * (max - min) + min);
+}
+
 const TypeHangul = {
     type: function (selector, options) {
 
