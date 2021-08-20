0a313070f1290abd7a9f6b2aaf3d33dbc58b378e
Bas950 <me@bas950.com>
2021-08-20 22:35:37 +0200

    feat: Typescript
    :sparkles: Typescript
    
    :bug: Export options
    
    :art: Fix source map
    
    :package:

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const webpack = require('webpack');
-const package = require('./package.json');
+const path = require("path");
+const webpack = require("webpack");
+const package = require("./package.json");
 
 const banner = `type-hangul v${package.version}
 https://github.com/SDuck4/type-hangul
@@ -9,31 +9,39 @@ MIT License
 Copyright (c) 2020 Chae SeungWoo`;
 
 const defaultConfig = {
-    mode: 'production',
-    entry: './src/index.js',
-    target: 'web',
-    output: {
-        filename: 'type-hangul.min.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    externals: {
-        'hangul-js': 'Hangul',
-    },
-    plugins: [
-        new webpack.BannerPlugin(banner),
-    ],
+	mode: "production",
+	entry: "./src/index.ts",
+	target: "web",
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader",
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: [".ts"]
+	},
+	output: {
+		filename: "type-hangul.min.js",
+		path: path.resolve(__dirname, "bin")
+	},
+	externals: {
+		"hangul-js": "Hangul"
+	},
+	plugins: [new webpack.BannerPlugin(banner)],
+	devtool: "source-map"
 };
 
 const bundleConfig = {
-    ...defaultConfig,
-    output: {
-        filename: 'type-hangul.bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    externals: [],
+	...defaultConfig,
+	output: {
+		filename: "type-hangul.bundle.js",
+		path: path.resolve(__dirname, "bin")
+	},
+	externals: []
 };
 
-module.exports = [
-    defaultConfig,
-    bundleConfig,
-];
+module.exports = [defaultConfig, bundleConfig];
